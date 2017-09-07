0ae883643bb1e73c09efd66219cf7110e549edfa
benjyrr <31515007+benjyrr@users.noreply.github.com>
2017-09-07 10:00:03 -0700

    splash
    lab

---
+++ Splash.java
@@ -0,0 +1,27 @@
+/*
+Author: Benjy R-R
+
+
+Last Edited: 9/7/17
+*/
+
+public class Splash{
+
+	//testing with main
+	public static void main(String[] args){
+		//call spash screen
+		splashScreen();
+	}
+	
+	//prints out custom splash screen
+	// remove static later
+	public static void splashScreen(){
+		System.out.println(
+			 " __"
+	 	+   " |__|____ ___  _______"   
+		+   " |  \__  \\  \/ /\__  \"  
+		+	" |  |/ __ \\   /  / __ \_"
+	    +"/\__|  (____  /\_/  (____  /"
+	    +"\______|    \/           \/ ");
+	}
+}
