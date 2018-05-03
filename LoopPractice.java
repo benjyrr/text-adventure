60dc1d748c8513845f98eb0840344d455ffedd4f
https://github.com/gwalker14/gwalker14.github.io.git <https://github.com/gwalker14/gwalker14.github.io.git>
2018-05-03 09:12:57 -0700

    Loops

---
+++ LoopPractice.java
@@ -0,0 +1,15 @@
+public class LoopPractice{
+
+	public static void main(String[] args){
+
+		int n = 6;
+		int j = n;
+
+	for(int i = 6; i < n; i++){
+
+		j = j * (n - i);
+
+		System.out.println(j);
+		}		
+	}
+}
